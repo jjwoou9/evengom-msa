c37339b5ad930f97663b7a472b52c5885cc75002
jeewoou <51470001+jjwoou9@users.noreply.github.com>
2022-08-07 19:12:51 +0900

    Initial commit

---
+++ user-service/src/main/java/com/evengom/userservice/security/WebSecurity.java
@@ -0,0 +1,55 @@
+package com.evengom.userservice.security;
+
+import com.evengom.userservice.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
+
+    public WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.env = env;
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        log.info("WebSecurity  configure http {}",  http);
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/users/**").permitAll()
+                .and()
+                .addFilter(getAuthenticationFilter());
+//		http.authorizeRequests().antMatchers("/**")
+//						.hasIpAddress("192.168.0.7")
+//						.and()
+//						.addFilter(getAuthenticationFilter());
+
+        http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter =
+                new AuthenticationFilter(authenticationManager(), userService, env);
+
+        return authenticationFilter;
+    }
+
+    // select pwd from users where email=?
+    // db_pwd(encrypted) == input_pwd(encrypted)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
+}
---
+++ user-service/src/main/java/com/evengom/userservice/service/UserServiceImpl.java
@@ -0,0 +1,101 @@
+package com.evengom.userservice.service;
+
+import com.evengom.userservice.dto.UserDto;
+import com.evengom.userservice.jpa.UserEntity;
+import com.evengom.userservice.jpa.UserRepository;
+import com.evengom.userservice.vo.ResponseOrder;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+    UserRepository userRepository;
+    BCryptPasswordEncoder passwordEncoder;
+    Environment env;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(username);
+
+        if (userEntity == null)
+            throw new UsernameNotFoundException(username + ": not found");
+
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPwd(),
+                true, true, true, true,
+                new ArrayList<>());
+    }
+
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, Environment env) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.env = env;
+    }
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        userDto.setUserId(UUID.randomUUID().toString());
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = mapper.map(userDto, UserEntity.class);
+        userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
+
+        userRepository.save(userEntity);
+
+        UserDto returnUserDto = mapper.map(userEntity, UserDto.class);
+
+        return returnUserDto;
+    }
+
+    @Override
+    public UserDto getUserByUserId(String userId) {
+        // DB에서 일치하는 조회
+        UserEntity userEntity = userRepository.findByUserId(userId);
+        //조회 결과 존재여부 확인
+        if(userEntity==null) {
+            throw new UsernameNotFoundException("일치하는 사용자 정보가 없습니다.");
+        }
+        // UserEntity->UserDto
+        UserDto userDto= new ModelMapper().map(userEntity, UserDto.class);
+
+        // DB에서 해당 사용자의 구매내역을 조회
+        List<ResponseOrder> orders=new ArrayList();
+        // DB에서 해당사용자의 구매내역을 조회 -> UserDto에 추가, 반환
+        userDto.setOrders(orders);
+
+        return userDto;
+    }
+
+    @Override
+    public Iterable<UserEntity> getUserByAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public UserDto getUserDetailsByEmail(String email) {
+        UserEntity userEntity = userRepository.findByEmail(email);
+        if (userEntity == null)
+            throw new UsernameNotFoundException(email);
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(userEntity, UserDto.class);
+        return userDto;
+    }
+}
