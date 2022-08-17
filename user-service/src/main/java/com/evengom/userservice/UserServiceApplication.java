5c2fb7d630532ba2f543de9af1357e85d4a91e8e
jeewoou <51470001+jjwoou9@users.noreply.github.com>
2022-08-17 23:06:43 +0900

    userService LoadBalance 주석 해제

--- user-service/src/main/java/com/evengom/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/evengom/userservice/UserServiceApplication.java
@@ -23,10 +23,10 @@ public class UserServiceApplication {
         return new BCryptPasswordEncoder();
     }
 
-/*    @Bean
+    @Bean
     @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
-    }*/
+    }
 
 }
